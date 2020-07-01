279b907de4bfdca6f704e91c26331d58603427a4
sweethoneybee <jsjphone8@gmail.com>
2020-07-01 23:03:46 +0900

    First commit

---
+++ Assets/Scripts/mush_move.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mush_move : MonoBehaviour
+{
+    
+    Animator animator;
+    bool isJump;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();    
+        isJump = false;
+        speed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f, 0f);
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if(Input.GetButtonDown("Fire1"))
+            isJump = !isJump;
+
+        animator.SetBool("isJump", isJump);  
+        
+        if(Input.GetButton("Horizontal")){
+            int dir;
+            Vector3 characterScale = transform.localScale;
+            if(h > 0){
+                dir = 1;
+                characterScale.x = -2;
+            }
+            else{
+                dir = -1;
+                characterScale.x = 2;
+            }
+            transform.Translate(new Vector3(dir, 0, 0) * speed * Time.deltaTime);
+
+            // flip
+            transform.localScale = characterScale;
+        }
+        // if(h < 0){
+        //     transform.Translate(Vector3.left * Time.deltaTime);
+        //     //transform.position = new Vector3(0, 0, transform.position.z);
+        // }
+
+    }
+}
+
