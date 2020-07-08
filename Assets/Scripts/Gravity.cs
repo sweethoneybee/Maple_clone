95404fdeb2db10f9448e4fcb109bb4c5f0e47526
sweethoneybee <jsjphone8@gmail.com>
2020-07-09 00:53:16 +0900

    Implement adjusting gravity scale and tried mush_jump using AddForce Can't use Rigidbody2D with BoxCollider. So, for preventing mushroom falls through the box, used BoxCollider and Rigidbody. Need to implements. 1. Make being able to jump only at the ground. 2. Make jump  naturally, not like in the Moon.

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public float gravity = -9.81f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity = new Vector3(0, gravity, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/mush_jump.cs
+++ Assets/Scripts/mush_jump.cs
@@ -6,20 +6,36 @@ public class mush_jump : MonoBehaviour
 {
     Animator animator;
     bool isJump;
-
+    Rigidbody rb;
+    public int jumpPower;
     void Start()
     {
         animator = GetComponent<Animator>();
-        isJump = false;    
+        isJump = false;
+        rb = GetComponent<Rigidbody>();
+        jumpPower = 2;
     }
 
+    private void FixedUpdate()
+    {
+        if(Input.GetButton("Vertical") == true && isJump == false)
+        {
+            isJump = true;
+            rb.AddForce(new Vector2(0, jumpPower), ForceMode.Impulse);
+        }
+
+        if(rb.velocity.y <= 0)
+        {
+            isJump = false;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        float v = Input.GetAxis("Vertical");
-        if (Input.GetButton("Vertical") == true)
-        { 
-            transform.Translate(new Vector3(0, v, 0) * Time.deltaTime);
-        }
+        //float v = Input.GetAxis("Vertical");
+        //if (Input.GetButton("Vertical") == true)
+        //{ 
+        //    transform.Translate(new Vector3(0, v, 0) * Time.deltaTime);
+        //}
     }
 }
