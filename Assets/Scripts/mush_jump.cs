ff137c65210f0008f68933d05ef534cd1da2a5a3
sweethoneybee <jsjphone8@gmail.com>
2020-07-08 16:25:27 +0900

    Try implement mush_jump

---
+++ Assets/Scripts/mush_jump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mush_jump : MonoBehaviour
+{
+    Animator animator;
+    bool isJump;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isJump = false;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        if (Input.GetButton("Vertical") == true)
+        { 
+            transform.Translate(new Vector3(0, v, 0) * Time.deltaTime);
+        }
+    }
+}
